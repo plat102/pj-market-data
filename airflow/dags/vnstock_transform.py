60dae1535010c5569cc6f26930c339cb2cd5674a
thupla <thuleanhphan@gmail.com>
2024-12-11 16:40:08 +0700

    [Airflow] feat: dag transforming stock

---
+++ airflow/dags/vnstock_transform.py
@@ -0,0 +1,49 @@
+"""Ingest data from VNStock and store it in S3"""
+
+import sys
+from datetime import timedelta, datetime
+
+from airflow.decorators import dag, task
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+default_args = {
+    "owner": "thu.phan",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+@dag(
+    dag_id="vnstock_transform",
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=datetime(2024, 12, 10),
+    catchup=False,
+    description="Transform raw .json data from VNStock and parquet in S3 silver layer",
+)
+def vnstock_data_transform():
+    """
+    """
+    
+    start = DummyOperator(task_id="start")
+
+    @task()
+    def pm_run_stock_notebook():
+        print('installing papermill')
+    
+    spark_job = SparkSubmitOperator(
+        task_id="spark_transform_job_stock",
+        application="/opt/airflow/spark/application/python/process_stock_price.py",
+        conn_id="spark_default",
+        name="arrow-spark",
+        conf={
+            "spark.executor.memory": "2g",
+            "spark.driver.memory": "2g",
+        },
+        verbose=True
+    )
+
+    start >> [pm_run_stock_notebook(), spark_job]
+
+
+vnstock_transform_dag = vnstock_data_transform()
