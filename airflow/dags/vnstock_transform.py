1b5be992fcfa74953c28f1075c69df6e4790b07e
thupla <thuleanhphan@gmail.com>
2024-12-17 21:36:04 +0700

    [spark & airflow] add load from silver to PostgreSQL step

--- airflow/dags/vnstock_transform.py
+++ airflow/dags/vnstock_transform.py
@@ -1,17 +1,36 @@
 """Ingest data from VNStock and store it in S3"""
-
-import sys
+import os
 from datetime import timedelta, datetime
+
+from dotenv import load_dotenv
 
 from airflow.decorators import dag, task
 from airflow.operators.dummy import DummyOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.models import Connection
+
+load_dotenv()
+
+BRONZE_PATH = os.environ.get("BRONZE_PATH")
+SILVER_PATH = os.environ.get("SILVER_PATH")
+MINIO_ACCESS_KEY = os.environ.get("MINIO_ACCESS_KEY")
+MINIO_SECRET_KEY = os.environ.get("MINIO_SECRET_KEY")
+
+bronze_path = f"s3a:/{BRONZE_PATH}/vnstock3/stock_quote_history_daily/"
+silver_path = f"s3a:/{SILVER_PATH}/vnstock3/daily_stock_prices/"
+
 
 default_args = {
     "owner": "thu.phan",
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
+
+# Retrieve PostgreSQL credentials from Airflow connection
+postgres_analytics_conn = Connection.get_connection_from_secrets("postgres_analytics")
+db_url = f"jdbc:postgresql://{postgres_analytics_conn.host}:{postgres_analytics_conn.port}/{postgres_analytics_conn.schema}"
+db_user = postgres_analytics_conn.login
+db_password = postgres_analytics_conn.password
 
 
 @dag(
@@ -31,16 +50,16 @@ def vnstock_data_transform():
     def pm_run_stock_notebook():
         print("installing papermill")
 
-    # TODO: SparkSubmitOperator to run the Spark job
-    spark_job = SparkSubmitOperator(
+    # SparkSubmitOperator to run the Spark job
+    process_stock_prices = SparkSubmitOperator(
         task_id="process_stock_prices",
         application="/opt/airflow/spark/application/python/process_stock_price.py",
         conn_id="spark_default",
-        jars='jars/delta-core_2.12-2.2.0.jar,jars/hadoop-aws-3.3.4.jar,jars/delta-storage-2.2.0.jar,jars/aws-java-sdk-1.12.367.jar,jars/s3-2.18.41.jar,jars/aws-java-sdk-bundle-1.11.1026.jar',
+        jars="jars/delta-core_2.12-2.2.0.jar,jars/hadoop-aws-3.3.4.jar,jars/delta-storage-2.2.0.jar,jars/aws-java-sdk-1.12.367.jar,jars/s3-2.18.41.jar,jars/aws-java-sdk-bundle-1.11.1026.jar",
         conf={
             "spark.hadoop.fs.s3a.endpoint": "http://minio:9000",
-            "spark.hadoop.fs.s3a.access.key": "minio",
-            "spark.hadoop.fs.s3a.secret.key": "minio123",
+            "spark.hadoop.fs.s3a.access.key": MINIO_ACCESS_KEY,
+            "spark.hadoop.fs.s3a.secret.key": MINIO_SECRET_KEY,
             "spark.hadoop.fs.s3a.path.style.access": "true",
             "spark.hadoop.fs.s3a.connection.ssl.enabled": "false",
             "spark.hadoop.fs.s3a.impl": "org.apache.hadoop.fs.s3a.S3AFileSystem",
@@ -51,7 +70,27 @@ def vnstock_data_transform():
         application_args=[],
     )
 
-    (start >> [pm_run_stock_notebook(), spark_job])
+    load_stock_prices_to_pg = SparkSubmitOperator(
+        task_id="load_stock_prices_to_pg",
+        application="/opt/airflow/spark/application/python/load_from_s3_to_pg.py",
+        name="ETL_Spark_Job",
+        conn_id="spark_default",
+        jars="jars/hadoop-aws-3.3.4.jar,jars/aws-java-sdk-1.12.367.jar,jars/s3-2.18.41.jar,jars/aws-java-sdk-bundle-1.11.1026.jar,jars/postgresql-42.5.0.jar",
+        conf={"spark.executor.memory": "1g", "spark.executor.cores": "1"},
+        application_args=[
+            "--file_key", silver_path,
+            "--app_name", "Stock_Parquet_to_PostgreSQL",
+            "--db_url", db_url,
+            "--db_user", db_user,
+            "--db_password", db_password,
+            "--db_schema", "staging",
+            "--db_table", "stock_prices_history",
+        ],
+    )
+    (
+        start >> [pm_run_stock_notebook(), process_stock_prices]
+        >> load_stock_prices_to_pg
+    )
 
 
 vnstock_transform_dag = vnstock_data_transform()
--- airflow/spark/application/python/load_from_s3_to_pg.py
+++ airflow/spark/application/python/load_from_s3_to_pg.py
@@ -1,8 +1,82 @@
+"""Load data from S3 silver layer to PostgreSQL using Spark"""
 
-# Load environment variables
+import argparse
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp
 
-# Create a SparkSession
+import os
+from dotenv import load_dotenv
 
-# Read data from S3
+load_dotenv()
 
-# Write data to PostgreSQL
+MINIO_ACCESS_KEY = os.environ.get("MINIO_ACCESS_KEY")
+MINIO_SECRET_KEY = os.environ.get("MINIO_SECRET_KEY")
+
+def run_spark_etl(bucket, file_key, app_name, db_url, db_user, db_password, db_schema, db_table):
+    # Initialize Spark Session
+    spark = SparkSession.builder \
+        .master("spark://spark-master:7077") \
+        .appName(app_name) \
+        .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
+        .config("spark.hadoop.fs.s3a.access.key", MINIO_ACCESS_KEY) \
+        .config("spark.hadoop.fs.s3a.secret.key", MINIO_SECRET_KEY) \
+        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+        .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.jars", "jars/hadoop-aws-3.3.4.jar,jars/aws-java-sdk-1.12.367.jar,jars/postgresql-42.5.0.jar") \
+        .getOrCreate()
+
+    # Extract: Read Parquet from S3
+    if file_key.startswith("s3a://"):
+        s3_path = file_key
+    elif not bucket:
+        s3_path = f"s3a://{bucket}/{file_key}"
+    else:
+        s3_path = f"s3a://{file_key}"
+    print(f"Reading data from: {s3_path}")
+    df = spark.read.parquet(s3_path)
+
+    # Load: Write to PostgreSQL
+    db_properties = {
+        "user": db_user,
+        "password": db_password,
+        "driver": "org.postgresql.Driver",
+    }
+    db_table_path = f"{db_schema}.{db_table}"
+    print(f"Writing data to: {db_url}, table: {db_table_path}")
+
+    df.write \
+        .jdbc(
+            url=db_url,
+            table=db_table_path,
+            mode="overwrite",
+            properties=db_properties,
+        )
+
+    print("ETL process completed successfully.")
+    
+    spark.stop()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--bucket", default=None, help="S3 bucket name (optional)")
+    parser.add_argument("--file_key", required=True, help="Parquet file key in S3")
+    parser.add_argument("--app_name", required=True, help="Spark application name")
+    parser.add_argument("--db_url", required=True, help="PostgreSQL JDBC URL")
+    parser.add_argument("--db_user", required=True, help="PostgreSQL user name")
+    parser.add_argument("--db_password", required=True, help="PostgreSQL user password")
+    parser.add_argument("--db_schema", required=True, help="PostgreSQL schema name")
+    parser.add_argument("--db_table", required=True, help="PostgreSQL table name")
+    args = parser.parse_args()
+
+    run_spark_etl(
+        bucket=args.bucket,
+        file_key=args.file_key,
+        app_name=args.app_name,
+        db_url=args.db_url,
+        db_user=args.db_user,
+        db_password=args.db_password,
+        db_schema=args.db_schema,
+        db_table=args.db_table
+    )
+    
