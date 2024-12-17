d57b733cd6e8e7ecebf4b4542ee691de31e57a9f
thupla <thuleanhphan@gmail.com>
2024-12-17 21:28:46 +0700

    [airflow] add local jars

--- download_jars.sh
+++ download_jars.sh
@@ -13,5 +13,6 @@ curl -o $JARS_DIR/delta-core_2.12-2.2.0.jar https://repo1.maven.org/maven2/io/de
 curl -o $JARS_DIR/delta-storage-2.2.0.jar https://repo1.maven.org/maven2/io/delta/delta-storage/2.2.0/delta-storage-2.2.0.jar
 curl -o $JARS_DIR/hadoop-aws-3.3.4.jar https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.4/hadoop-aws-3.3.4.jar
 curl -o $JARS_DIR/aws-java-sdk-bundle-1.11.1026.jar https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.11.1026/aws-java-sdk-bundle-1.11.1026.jar
+curl -o $JARS_DIR/postgresql-42.5.0.jar https://repo1.maven.org/maven2/org/postgresql/postgresql/42.5.0/postgresql-42.5.0.jar
 
 echo "JAR files downloaded to $JARS_DIR"
